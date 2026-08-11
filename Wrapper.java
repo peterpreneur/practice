f863ad3c926281c9c99f3f4b59b436ffd8ab8f8a
Peter S <peterpreneur@gmail.com>
2026-08-11 17:02:21 -0500

    test

---
+++ Wrapper.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package org.peterpreneur;
+
+/**
+ *
+ * @author peter
+ */
+public class Wrapper {
+    public static void main(String[] args) {
+        //Autoboxing
+
+        //Unboxing
+
+        //Wrapper can accept null value
+
+        //Collections
+
+        //Type conversion - from one type to another
+
+        //the main advantanges of using wrapper is that it can be use in Collections
+
+        
+    }
+
+}
