64c7c039de5d9560243779eb267fcea327795ca4
Peter S <peterpreneur@gmail.com>
2026-08-12 15:00:41 -0500

    test

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -4,9 +4,18 @@
  */
 package org.peterpreneur;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+class Student {
+
+    int rollno;
+    String name;
+    int age;
+
+    Student(int rollno, String name, int age) {
+        this.rollno = rollno;
+        this.name = name;
+        this.age = age;
+    }
+}
 
 /**
  *
@@ -46,9 +55,22 @@ public class ArrayListDemo {
         ArrayList<Object> myList7 = new ArrayList<>();
         myList7.add(10);
 
-        myList.removeAll(myList7); 
+        myList.removeAll(myList7);
         System.out.println(myList);
-        
-        
+
+        Student stu1 = new Student(010, "Peter", 50);
+        Student stu2 = new Student(011, "Sunny", 49);
+        Student stu3 = new Student(012, "Gisele", 17);
+
+        ArrayList<Student> myStudent = new ArrayList<>();
+        myStudent.add(stu1);
+        myStudent.add(stu2);
+        myStudent.add(stu3);
+
+        Iterator itr = myStudent.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr);
+        }
     }
 }
